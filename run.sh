4df0cd9dee1340ab1282c2a656a47e0535c5fb88
ExecuteAutomation <kartmcad@gmail.com>
2022-01-17 15:58:02 +1300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+echo "Running from Shell script"
+echo "List all directory of the runner machine"
+ls -l
+
+cd ..
+
+echo "Getting out from the directory and listing files"
+
+ls - l
